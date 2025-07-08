96aaab5a30cb9178d1883ab4fd8d76fb10c41716
Vikram <vikraminps@gmail.com>
2025-07-08 07:46:43 +0530

    commit updated

--- resources/views/ui-paginations.blade.php
+++ resources/views/ui-paginations.blade.php
@@ -1051,10 +1051,9 @@
         <!-- Javascript  -->  
         <!-- vendor js -->
         
-        <script src="assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
-        <script src="assets/libs/simplebar/simplebar.min.js"></script>
-
-        <script src="assets/js/app.js"></script>
+        <script src="{{ asset('assets/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+        <script src="{{ asset('assets/libs/simplebar/simplebar.min.js')}}"></script>
+        <script src="{{ asset('assets/js/app.js')}}"></script>
     </body>
     <!--end body-->
 </html>
